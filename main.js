273b3c7437620759253f2fa93248d452a93750c1
Geoph88 <geoffrey_sizgoric@hotmail.com>
2022-07-11 17:49:27 +1000

    adding all other files

---
+++ main.js
@@ -0,0 +1,8 @@
+// const getNavLinks = document.querySelectorAll('.nav_link')
+
+// console.log(getNavLinks)
+// getNavLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     document.body.classList.remove('nav-open')
+//   })
+  
