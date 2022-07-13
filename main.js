4a935b324f2a46b5b7167e6b2a101478eb91d054
Geoph88 <geoffrey_sizgoric@hotmail.com>
2022-07-13 10:08:53 +1000

    fixed about me section and deleted extra images

--- main.js
+++
@@ -1,8 +0,0 @@
-// const getNavLinks = document.querySelectorAll('.nav_link')
-
-// console.log(getNavLinks)
-// getNavLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     document.body.classList.remove('nav-open')
-//   })
-  
